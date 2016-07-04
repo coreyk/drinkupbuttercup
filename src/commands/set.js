7de74365d63c2883a7f81096b77983e0f8c208b9
coreyk <accts@donut.org>
2016-07-04 14:24:15 -0400

    num

--- src/commands/set.js
+++ src/commands/set.js
@@ -93,7 +93,7 @@ const handler = (payload, res) => {
       tap: arr[1],
       name: arr[2],
       url: resp.items[0].link || "",
-      abv: arr[3] || "",
+      abv: arr[3] || "n/a",
       size: arr[4] || 5
     };
 
