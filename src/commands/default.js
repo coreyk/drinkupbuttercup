38b647354a1dec0f7cd98e354cf6552dfe0825cc
coreyk <accts@donut.org>
2016-07-04 02:20:03 -0400

    db

--- src/commands/default.js
+++ src/commands/default.js
@@ -14,6 +14,7 @@ const msgDefaults = {
 }
 
 const handler = (payload, res) => {
+
   co(function*() {
     var db = yield mongodb.MongoClient.connect(config('MONGODB_URI'));
     console.log("Connected correctly to server");
@@ -21,12 +22,28 @@ const handler = (payload, res) => {
     var col = db.collection('beers');
 
     var taps = [1, 2];
-    var beers = [];
-    taps.forEach((tap) => {
-      var beer = yield col.find({tap: tap}).limit(1).sort({_id: -1});
-      assert.equal(1, beer.length);
-      beers.push(beer);
-    })
+    // var beers = [];
+    // taps.forEach((tap) => {
+    //   var beer = yield col.find({tap: tap}).limit(1).sort({_id: -1});
+    //   assert.equal(1, beer.length);
+    //   beers.push(beer);
+    // })
+    let beers = [
+      {
+        tap: 1,
+        name: 'Other Half All Citra Everything IPA',
+        url: 'http://www.beeradvocate.com/beer/profile/33510/220357/',
+        abv: 8.5,
+        size: 5
+      },
+      {
+        tap: 2,
+        name: 'Brooklyn Lager',
+        url: 'http://www.beeradvocate.com/beer/profile/45/148/',
+        abv: 5.2,
+        size: 5
+      }
+    ]
 
     let attachments = beers.map((beer) => {
       return {
