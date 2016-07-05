2f2a69cef763b996ef1a612ee23b42cea8a7fb5c
coreyk <accts@donut.org>
2016-07-05 15:37:12 -0400

    date

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,11 +3,17 @@ exports.dateFromObjectId = (objectId) => {
 };
 
 exports.daysBetween = (date1, date2) => {
-  const ONE_DAY = 1000 * 60 * 60 * 24
-  var date1_ms = date1.getTime()
-  var date2_ms = date2.getTime()
-  var difference_ms = Math.abs(date1_ms - date2_ms)
-  return Math.ceil(difference_ms/ONE_DAY)
+  // const ONE_DAY = 1000 * 60 * 60 * 24
+  // var date1_ms = date1.getTime()
+  // var date2_ms = date2.getTime()
+  // var difference_ms = Math.abs(date1_ms - date2_ms)
+  // return Math.ceil(difference_ms/ONE_DAY)
+
+  var oneDay = 24*60*60*1000;
+  var firstDate = new Date(date1);
+  var secondDate = new Date(date2);
+  var diffDays = Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay));
+  return diffDays;
 }
 
 exports.daysOnTap = (date) => {
