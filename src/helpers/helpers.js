0a681b9156edbc5d952313f49da92de1ececeec0
coreyk <accts@donut.org>
2016-08-18 15:37:48 -0400

    more day precision

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -8,12 +8,14 @@ exports.daysBetween = (date1, date2) => {
   var oneDay = 24*60*60*1000;
   var firstDate = new Date(date1);
   var secondDate = new Date(date2);
-  var diffDays = Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay));
+	var diffDays = Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay));
+	console.log(diffDays);
   return diffDays;
 }
 
 exports.daysOnTap = (date) => {
-  return Math.ceil(exports.daysBetween(date, Date.now()));
+	// return Math.ceil(exports.daysBetween(date, Date.now()));
+  return parseFloat(exports.daysBetween(date, Date.now())).toFixed(1);
 }
 
 exports.isBlank = (value) => {
